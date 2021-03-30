007d8bf4810e807ad8b41c4afe1540f06d378332
Colsen Selk <ceselk@iastate.edu>
2021-03-30 16:51:25 -0500

    minor update for api

--- sentenceSeekersJavaCode/src/sentenceSeekersJavaCode/Sentence.java
+++ sentenceSeekersJavaCode/src/sentenceSeekersJavaCode/Sentence.java
@@ -37,14 +37,19 @@ public class Sentence {
 		 numAdverb = tempScan.nextInt();
 		
 		 File myObj = new File("websiteinformation.txt");
+		 
 		 FileWriter myWriter = new FileWriter("websiteinformation.txt");
 		 String websitename = "http://merriam-webster.com/dictionary/" + tempScan.next();
 		 String tempstring;
 			ScraperApiClient client = new ScraperApiClient("f5e450d823882f2efdb356a6c352443a");
 			  tempstring = (client.get(websitename)
 			  .result()).toString();
-		 System.out.println(tempstring);
-		myWriter.close();
+		 // test tempstring to see if it includes /noun /verb /adjective and/or /adverb
+		 // assign tempstring to be at random something it is, or if there is already 2 of some word, make different word.
+		 myWriter.close();
+		 myObj.delete();
+		 myObj.createNewFile();
+		 
 		 
 		 /**
 		  * Assigns variables based on how many variables there are.
